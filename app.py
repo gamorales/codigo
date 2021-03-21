a9681fe4e9e7097897feea62de642bd933f2d70c
Guillermo Morales <guilleccep@gmail.com>
2021-03-21 15:33:25 -0500

    app.py modificado con f-string

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 import os
 
-print(os.close())
-
-
 segundos=int(input("Escriba una cantidad de segundos"))
 horas=segundos//3600
 sobrante1=segundos%3600
 minutos=sobrante1//60
 sobrante2=sobrante1%60
 
-print("segundos equivalen a:",horas,"Hora(s)",minutos,"minutos","y",sobrante2,"segundos")
-
+print(f"""Tiempo equivale a: {segundos}
+        Hora(s): {horas}
+        Minuto(s): {minutos}
+        Segundo(s): {sobrante2}""")
