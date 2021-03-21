baa8244eeac3433814d80f44aac1e0a9b4512cd3
Guillermo Morales <guilleccep@gmail.com>
2021-03-21 15:02:13 -0500

    Modificado el archivo .gitignore con php y swp files. Modificado el app.py

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import os
+
+
 segundos=int(input("Escriba una cantidad de segundos"))
 horas=segundos//3600
 sobrante1=segundos%3600
@@ -5,3 +8,4 @@ minutos=sobrante1//60
 sobrante2=sobrante1%60
 
 print("segundos equivalen a:",horas,"Hora(s)",minutos,"minutos","y",sobrante2,"segundos")
+
